780be0479f380b1ffc9d4aaeea7dcd4416c20b83
mwoodiwiss <malachaiwoodiwiss1999@gmail.com>
2019-08-27 19:26:46 -0600

    styled-project

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -10,7 +10,7 @@ const Operators = () => {
   // STEP 2 - add the imported data to state
   const [buttonOperators, setButtonOperator] = useState(operators);
   return (
-    <div className='operators'>
+    <div className={'opButtons'}>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
